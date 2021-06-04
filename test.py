b467300dbc2188a5b6c39fd9116a4f3ab993fefc
Somesh Daga <s2daga@uwaterloo.ca>
2021-06-03 21:53:50 -0400

    Minor change to scratch test scripts

--- test.py
+++ test.py
@@ -1,17 +1,46 @@
 import cProfile
-import seqopt.environments
-import gym
+
+import robosuite as suite
+from robosuite import load_controller_config
+from robosuite.wrappers import GymWrapper
 import time
 
-env = gym.make('kitchen_relax-v1')
-_ = env.reset()
 
-def tic_toc(env, steps=1000):
-    s = time.time()
-    for _ in range(steps):
-        act = env.action_space.sample()
-        _ = env.step(act)
-    return time.time() - s
+if __name__ == "__main__":
 
-print(f"Total time: {tic_toc(env)}")
-#cProfile.run("env.step(env.action_space.sample())")
+    RENDER = False
+
+    # Load OSC controller configuration
+    osc_config = load_controller_config(default_controller='OSC_POSE')
+
+    # Notice how the environment is wrapped by the wrapper
+    env = GymWrapper(
+        suite.make(
+            "Door",
+            robots="Jaco",                  # use WAM robot
+            controller_configs=osc_config,  # OSC Controller Configuration
+            use_camera_obs=False,           # do not use pixel observations
+            has_offscreen_renderer=False,   # not needed since not using pixel obs
+            has_renderer=RENDER,              # make sure we can render to the screen
+            reward_shaping=True,            # use dense rewards
+            control_freq=20,                # control should happen fast enough so that simulation looks smooth
+        )
+    )
+
+    def sim_time_test():
+        sim_start_time = time.time()
+        # Run for 1000 steps
+        observation = env.reset()
+        for t in range(1000):
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                print(f"Episode finished after {t + 1} timesteps")
+                break
+            if RENDER:
+                env.render()
+        return time.time() - sim_start_time
+
+    cProfile.run("sim_time_test()")
+    print(f"Simulation time for 1000 steps: {sim_time_test()} s")
+
